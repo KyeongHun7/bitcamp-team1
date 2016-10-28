d62fe6cda39a5c075c09da68c6f57daeb1a1567e
unknown <>
2016-10-28 15:34:02 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -26,7 +26,8 @@ public class EduApp {
         case "go 1" : bookController.service(); break;
         case "go 2" : teacherController.service(); break;
         case "go 3" : lecturecontroller.service(); break;
-        case "help1" : break; 
+        case "help1" : break;
+        case "help2" : break;
         case "quit" :
          System.out.println("잘가용");
          break loop;
