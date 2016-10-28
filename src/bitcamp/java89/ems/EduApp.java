cea4925ca9ce5076e256148c2ce56a7b43a6e893
geon woo kim <simsimhea409@naver.com>
2016-10-28 15:44:41 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -18,6 +18,9 @@ public class EduApp {
     System.out.println("지원하지 않는 명령어 입니다.");
     System.out.println("지원하지 않는 명령어 입니다.");
     System.out.println("지원하지 않는 명령어 입니다.");
+
+    System.out.println("지원하지 않는 명령어 입니다.");
+    System.out.println("지원하지 않는 명령어 입니다.");
     System.out.println("지원하지 않는 명령어 입니다.");
     System.out.println("지원하지 않는 명령어 입니다.");
     System.out.println("지원하지 않는 명령어 입니다.");
